2c68ed8b15843a88df6a0004efe7da4e754fe849
imtiyazatwork03 <imtiyazatwork03@gmail.com>
2022-12-25 19:51:27 +0530

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { ListBooks } from './pages/ListBooks';
@@ -6,7 +7,6 @@ import { useDispatch } from 'react-redux';
 import { getProfile, getState } from './store/actions/auth.action';
 import { useEffect } from 'react';
 import LogIn from './pages/LogIn';
-import Register from './pages/Register';
 import RegisterSeller from './pages/RegisterSeller';
 import ForgotPassword from './pages/ForgotPassword';
 import NotFound from './pages/NotFound';
@@ -22,6 +22,7 @@ import ContactUs from './pages/ContactUs';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import LandingView from './pages/LandingView';
 import SearchBook from './components/search/SearchBook';
+const Register = lazy(() => import('./pages/Register'));
 
 const App = () => {
     const dispatch = useDispatch();
@@ -34,42 +35,44 @@ const App = () => {
     }, [dispatch, token]);
     return (
         <Router>
-            <Routes>
-                <Route element={<PrivateRoutes />}>
-                    <Route path="/editmembership">
-                        <Route index={true} element={<ProfileUpdate />} />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route element={<PrivateRoutes />}>
+                        <Route path="/editmembership">
+                            <Route index={true} element={<ProfileUpdate />} />
+                        </Route>
+                        <Route index={false} element={<ListBooks />} path="/listbooks" />
+                        <Route index={false} element={<ListBooks />} path="/listbooks/:id" />
+                        <Route index={false} element={<MyListingBooks />} path="/managebooks" />
+                        <Route index={false} element={<Cart />} path="/mycart" />
                     </Route>
-                    <Route index={false} element={<ListBooks />} path="/listbooks" />
-                    <Route index={false} element={<ListBooks />} path="/listbooks/:id" />
-                    <Route index={false} element={<MyListingBooks />} path="/managebooks" />
-                    <Route index={false} element={<Cart />} path="/mycart" />
-                </Route>
-                <Route element={<LandingView />} path="/" />
-                <Route element={<Register />} path="/joinnow" />
-                <Route element={<RegisterSeller />} path="/register" />
-                <Route element={<LogIn />} path="/login" />
-                <Route element={<SearchBook />} path="searchbook" />
-                <Route element={<ForgotPassword />} path="/forgetpassword" />
-                <Route element={<NotFound />} path="/notfound" />
-                <Route element={<UnderConstruction />} path="/underconstruction" />
-                <Route element={<AboutUs />} path="/aboutus" />
-                <Route element={<Updates />} path="/updates" />
-                <Route element={<ContactUs />} path="/contactus" />
-                <Route element={<UserAgreement />} path="/useragreement" />
-                <Route element={<PrivacyPolicy />} path="/privacypolicy" />
-                <Route path="*" element={<LandingView />} />
-            </Routes>
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={true}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
+                    <Route element={<LandingView />} path="/" />
+                    <Route element={<Register />} path="/joinnow" />
+                    <Route element={<RegisterSeller />} path="/register" />
+                    <Route element={<LogIn />} path="/login" />
+                    <Route element={<SearchBook />} path="searchbook" />
+                    <Route element={<ForgotPassword />} path="/forgetpassword" />
+                    <Route element={<NotFound />} path="/notfound" />
+                    <Route element={<UnderConstruction />} path="/underconstruction" />
+                    <Route element={<AboutUs />} path="/aboutus" />
+                    <Route element={<Updates />} path="/updates" />
+                    <Route element={<ContactUs />} path="/contactus" />
+                    <Route element={<UserAgreement />} path="/useragreement" />
+                    <Route element={<PrivacyPolicy />} path="/privacypolicy" />
+                    <Route path="*" element={<LandingView />} />
+                </Routes>
+                <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={true}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
+            </Suspense>
         </Router>
     );
 }
